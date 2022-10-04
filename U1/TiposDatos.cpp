c633ae5a9f61f1074c4ea5a358c11067b48eef3a
UP210614 <up210614@alumnos.upa.edu.mx>
2022-10-04 12:20:12 -0500

    Hola

--- U1/TiposDatos.cpp
+++ U1/TiposDatos.cpp
@@ -1,7 +1,7 @@
 /*  Unidad 1. Tipos de datos
     Autor: Edgar Sandoval Rodríguez
     Fecha: 15/09/2022
-    Descripción: Muestra los diferentes tipos de datos en c++ hola
+    Descripción: Muestra los diferentes tipos de datos en c++ 
 */
 
 #include <iostream>
